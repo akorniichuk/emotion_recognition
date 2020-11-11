8aa54e856ffb827eff02c2bccddc408a46bcd125
Anatolii Korniichuk <anatolii.korniichuk@gmail.com>
2020-11-11 17:22:35 -0500

    final version

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, Response
+from flask import Flask, request, render_template, Response, jsonify
 from camera import LocalCamera
+import cv2
+from model import Expressions
+import numpy as np
+import base64
 
+face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+model = Expressions("model_2.json", "model_weights.h5")
+font = cv2.FONT_ITALIC
 
 app = Flask(__name__)
 
@@ -8,16 +15,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/input', methods=['POST'])
+def process_input():
+    image_b64 = request.json['image'].split(",")[1]
+    binary = base64.b64decode(image_b64)
+    image = np.asarray(bytearray(binary), dtype="uint8")
+    fr = cv2.imdecode(image, cv2.COLOR_BGR2GRAY) # cv2.IMREAD_COLOR in OpenCV 3.1
+    gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+    faces = face.detectMultiScale(gray_fr, 1.3, 5)
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(LocalCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    for (x, y, w, h) in faces:
+        fc = gray_fr[y:y+h, x:x+w]
+
+        roi = cv2.resize(fc, (48, 48))
+        pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+        padding = 35
+        cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+        cv2.rectangle(fr,(x-padding,y+padding),(x+w+padding,y+h+padding),(255,0,0),2)
+
+    _, jpeg = cv2.imencode('.jpg', fr)
+    img_base64 = base64.b64encode(jpeg.tobytes())
+
+    return jsonify({'status':str(img_base64)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
